0eb20cfea77daa540126648c9170e42f22188ef2
juhee <fhqj21@gmail.com>
2022-08-17 23:26:50 +0900

    learn about servlet

--- src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -16,7 +16,22 @@ public class RequestParamServlet extends HttpServlet {
 
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("RequestParamServlet.service");
+        System.out.println("[전체 파라미터 조회] - start");
+        req.getParameterNames().asIterator()
+                        .forEachRemaining(paramNAme -> System.out.println(paramNAme+"="+req.getParameter(paramNAme)));
+        
+        System.out.println("[전체 파라미터 조회] - end");
+        System.out.println();
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = req.getParameter("username");
+        String age = req.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age= "+age);
+
+        System.out.println();
+
+
         
     }
 }
